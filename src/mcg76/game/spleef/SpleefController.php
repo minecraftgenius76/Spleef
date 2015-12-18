5cfce7e77c51535d5cf971743ed7f16a04104574
BuildTools <mattwattz@gmail.com>
2015-12-18 14:18:21 +0000

    I have no idea what I am doing!

--- src/mcg76/game/spleef/SpleefController.php
+++ src/mcg76/game/spleef/SpleefController.php
@@ -452,8 +452,6 @@ class SpleefController extends MiniGameBase {
 			$message = $message = $this->getMsg ( "teleporting.spleef.location" ) . "-" . $levelhome;
 			$player->sendMessage ( $message );
 			$player->teleport ( $homePos );
-			// disable getting kicked for flying I hope
-			$player->resetFallDistance();
 		}
 	}
 	
--- src/mcg76/game/spleef/SpleefListener.php
+++ src/mcg76/game/spleef/SpleefListener.php
@@ -48,11 +48,8 @@ use pocketmine\item\ItemBlock;
  *      
  */
 class SpleefListener extends MiniGameBase implements Listener {
-	private $spleefWorldName;
-	
 	public function __construct(SpleefPlugin $plugin) {
 		parent::__construct ( $plugin );
-		$this->spleefWorldName = $this->getSetup ()->getHomeWorldName ();
 	}
 	
 	/**
@@ -135,24 +132,27 @@ class SpleefListener extends MiniGameBase implements Listener {
 	 */
 	public function onPlayerMove(PlayerMoveEvent $event) {
 		$player = $event->getPlayer ();
-		if( ! $player instanceof Player ) {
-			return;
+		$spleefworld = $this->getSetup ()->getHomeWorldName ();
+		
+		if( !is_null($event->getTo()->getLevel()) ) {
+			$toworld = $event->getTo()->getLevel()->getName();
+		} else {
+			// if getto world has not been supplied (null) it is safe to assume player world is the dest?
+			if(!is_null($player->getLevel())) {
+				$toworld = $player->getLevel()->getName();
+			} else {
+				// not sure if there is a possibilty we could catch an offline player with this event causing null?
+				$toworld = "";
+			}
 		}
-		if( ! isset( $event->getTo() ) ) {
-			return;
+		
+		if ($player instanceof Player && $toworld == $spleefworld) {
+			$v = round ( $event->getTo ()->x ) . "," . round ( $event->getTo ()->y ) . "," . round ( $event->getTo ()->z );
+			$this->getController ()->trackArenaPlayers ( $player, $v );
+		} else {
+			// shouldnt be needed in any scenario?
+			//$this->getController ()->trackArenaPlayers ( $player, "0" );
 		}
-		if( ! isset( $event->getTo()->getLevel() ) ) {
-			return;
-		}
-		$moveto = $event->getTo();
-		$spleefworld = $this->spleefWorldName;
-		$toworld = $event->getTo()->getLevel()->getName();
-		if( ! $spleefworld == $toworld ) {
-			return;
-		}
-
-		$v = round ( $moveto->x ) . "," . round ( $moveto->y ) . "," . round ( $moveto->z );
-		$this->getController ()->trackArenaPlayers ( $player, $v );
 	}
 	
 	/**
@@ -161,7 +161,28 @@ class SpleefListener extends MiniGameBase implements Listener {
 	 * @param EntityTeleportEvent $event        	
 	 */
 	public function onPlayerTeleport(EntityTeleportEvent $event) {
-		return $this->onPlayerMove($event);
+		$player = $event->getEntity();
+		if (!($player instanceof Player)) return;
+
+		if( !is_null($event->getTo()->getLevel()) ) {
+			$toworld = $event->getTo()->getLevel()->getName();
+		} else {
+			// if getto world has not been supplied (null) it is safe to assume player world is the dest?
+			if(!is_null($player->getLevel())) {
+				$toworld = $player->getLevel()->getName();
+			} else {
+				// not sure if there is a possibilty we could catch an offline player with this event causing null?
+				$toworld = "";
+			}
+		}
+		$spleefworld = $this->getSetup ()->getHomeWorldName ();
+
+		if($toworld == $spleefworld) {
+			$v = round ( $event->getTo ()->x ) . "," . round ( $event->getTo ()->y ) . "," . round ( $event->getTo ()->z );
+			$this->getController ()->trackArenaPlayers ( $player, $v );
+		} else {
+			$this->getController ()->trackArenaPlayers ( $player, "0" );
+		}
 	}
 	
 	/**
